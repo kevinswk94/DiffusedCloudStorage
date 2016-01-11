80146f8493bf660e6a83cef0cedffe20964b6edd
kevinswk94 <kevinswk94@gmail.com>
2016-01-11 11:50:53 +0800

    Reformateted MainWindow class

--- ui/ui/src/MainWindow.java
+++ ui/ui/src/MainWindow.java
@@ -146,11 +146,11 @@ public class MainWindow extends JFrame
 				try
 				{
 					// Generates large prime _p, random int _alpha and PRF key _k
-					 _p = generateLargePrime();
-					 _alpha = generateSecureRandomInteger(_p);
-					 _key = generatePRFKeyK();
-					
-					 // Displays them on the GUI
+					_p = generateLargePrime();
+					_alpha = generateSecureRandomInteger(_p);
+					_key = generatePRFKeyK();
+
+					// Displays them on the GUI
 					tb_prime.setText(String.valueOf(_p));
 					tb_alpha.setText(String.valueOf(_alpha));
 					tb_prfkey.setText(String.valueOf(_key));
@@ -158,15 +158,15 @@ public class MainWindow extends JFrame
 					// Erasure encodes the input file and returns a list of the InputStreams generated
 					// InputStreams will become empty after using it in a method
 					List<InputStream> encodedSlices = getErasureEncodedFileSlices(_inputFile);
-					
+
 					// Convert the InputStreams into Byte Arrays
 					List<byte[]> encodedSliceBytes = convertInputStreamToByteArray(encodedSlices);
-					
+
 					// Generate list of authenticators using byte arrays
 					List<Long> listOfAuthenticators = new ArrayList<Long>();
 					for (byte[] ba : encodedSliceBytes)
 						listOfAuthenticators.add(calculateAuthenticationValue(ba));
-					
+
 					// Create challenge set of random coefficients
 					List<Integer> Q = new ArrayList<Integer>();
 					for (int i = 1; i <= listOfAuthenticators.size(); i++)
@@ -174,7 +174,7 @@ public class MainWindow extends JFrame
 
 					// Sends Q and file blocks to the prover. Get the response back.
 					List<Long> response = prover(Q, encodedSliceBytes, listOfAuthenticators);
-					
+
 					// Determines if the response is verified and displays result on GUI
 					boolean verified = verifier(response, Q);
 					if (verified)
@@ -190,71 +190,71 @@ public class MainWindow extends JFrame
 		});
 		panel_manipulationControls.setLayout(null);
 		panel_manipulationControls.add(btn_verify);
-		
+
 		tb_alpha = new JTextField();
 		tb_alpha.setEditable(false);
 		tb_alpha.setBounds(203, 82, 145, 20);
 		panel_manipulationControls.add(tb_alpha);
 		tb_alpha.setColumns(10);
-		
+
 		tb_prfkey = new JTextField();
 		tb_prfkey.setEditable(false);
 		tb_prfkey.setBounds(203, 49, 145, 20);
 		panel_manipulationControls.add(tb_prfkey);
 		tb_prfkey.setColumns(10);
-		
+
 		lbl_Alpha = new JLabel("Alpha:");
 		lbl_Alpha.setBounds(49, 49, 83, 20);
 		panel_manipulationControls.add(lbl_Alpha);
-		
+
 		lbl_PrfKey = new JLabel("PRF Key:");
 		lbl_PrfKey.setBounds(49, 82, 83, 20);
 		panel_manipulationControls.add(lbl_PrfKey);
-		
+
 		lbl_sigma = new JLabel("Sigma:");
 		lbl_sigma.setBounds(49, 113, 54, 20);
 		panel_manipulationControls.add(lbl_sigma);
-		
+
 		tb_sigma = new JTextField();
 		tb_sigma.setEditable(false);
 		tb_sigma.setColumns(10);
 		tb_sigma.setBounds(203, 113, 145, 20);
 		panel_manipulationControls.add(tb_sigma);
-		
+
 		lbl_mu = new JLabel("Mu:");
 		lbl_mu.setBounds(49, 144, 54, 20);
 		panel_manipulationControls.add(lbl_mu);
-		
+
 		tb_mu = new JTextField();
 		tb_mu.setEditable(false);
 		tb_mu.setColumns(10);
 		tb_mu.setBounds(203, 144, 145, 20);
 		panel_manipulationControls.add(tb_mu);
-		
+
 		lbl_computedSigma = new JLabel("Calculated Sigma:");
 		lbl_computedSigma.setBounds(49, 175, 124, 20);
 		panel_manipulationControls.add(lbl_computedSigma);
-		
+
 		tb_calculatedSigma = new JTextField();
 		tb_calculatedSigma.setEditable(false);
 		tb_calculatedSigma.setColumns(10);
 		tb_calculatedSigma.setBounds(203, 175, 145, 20);
 		panel_manipulationControls.add(tb_calculatedSigma);
-		
+
 		lbl_prime = new JLabel("Prime:");
 		lbl_prime.setBounds(49, 18, 83, 20);
 		panel_manipulationControls.add(lbl_prime);
-		
+
 		tb_prime = new JTextField();
 		tb_prime.setEditable(false);
 		tb_prime.setColumns(10);
 		tb_prime.setBounds(203, 18, 145, 20);
 		panel_manipulationControls.add(tb_prime);
-		
+
 		lbl_result = new JLabel("Result:");
 		lbl_result.setBounds(49, 206, 54, 20);
 		panel_manipulationControls.add(lbl_result);
-		
+
 		tb_result = new JTextField();
 		tb_result.setEditable(false);
 		tb_result.setColumns(10);
@@ -289,7 +289,7 @@ public class MainWindow extends JFrame
 		}
 
 		String matrix = sb.toString();
-		//System.out.println("Matrix: " + matrix);
+		// System.out.println("Matrix: " + matrix);
 
 		// Declare an instance of the IDA algorithm
 		IInfoDispersal iid = new RabinImpl2();
@@ -310,7 +310,7 @@ public class MainWindow extends JFrame
 			lsSegmented = iid.split(fileInputStream, info);
 
 			// Display number of slices generated
-			//System.out.println("Output Segments : " + lsSegmented.size());
+			// System.out.println("Output Segments : " + lsSegmented.size());
 
 			lsSegmentedWithId = new ArrayList<InputStream>();
 			byte index = 0; // byte can contain value from -128 to 127
@@ -320,12 +320,10 @@ public class MainWindow extends JFrame
 			for (InputStream inputStream : lsSegmented)
 				lsSegmentedWithId.add(new SequenceInputStream(new ByteArrayInputStream(new byte[] { index++ }), inputStream));
 
-		}
-		catch (FileNotFoundException e)
+		} catch (FileNotFoundException e)
 		{
 			e.printStackTrace();
-		}
-		catch (IDAException e)
+		} catch (IDAException e)
 		{
 			e.printStackTrace();
 		}
@@ -333,38 +331,40 @@ public class MainWindow extends JFrame
 		return lsSegmentedWithId;
 		// return lsSegmented;
 	}
- 	
+
 	/**
 	 * Takes a list of InputStreams and convert them into byte arrays, as the Input Streams clear after they are read
+	 * 
 	 * @param listOfInputStreams
 	 * @return List of byte arrays
 	 */
 	private List<byte[]> convertInputStreamToByteArray(List<InputStream> listOfInputStreams)
 	{
 		List<byte[]> listOfByteArrays = new ArrayList<byte[]>();
-		
+
 		for (InputStream is : listOfInputStreams)
 		{
 			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-			
+
 			try
 			{
 				int oneByte;
 				while ((oneByte = is.read()) != -1)
 					buffer.write(oneByte);
-				
+
 			} catch (Exception ex)
 			{
 				ex.printStackTrace();
 			}
 			listOfByteArrays.add(buffer.toByteArray());
 		}
-		
+
 		return listOfByteArrays;
 	}
 
 	/**
 	 * Gets randomly chosen n number of items from a list
+	 * 
 	 * @param lst List of InputStreams
 	 * @param n number of elements to be randomly chosen
 	 * @return Returns a list of randomly picked InputStreams
@@ -378,6 +378,7 @@ public class MainWindow extends JFrame
 
 	/**
 	 * Generates a large prime using a couple of SecureRandom instances
+	 * 
 	 * @return Returns a large prime p
 	 */
 	private int generateLargePrime()
@@ -421,6 +422,7 @@ public class MainWindow extends JFrame
 
 	/**
 	 * Generates a secure random value between 0 and bound
+	 * 
 	 * @param bound The upper limit
 	 * @return Returns a securely generated random integer
 	 */
@@ -448,6 +450,7 @@ public class MainWindow extends JFrame
 
 	/**
 	 * Generates a random integer to be used as PRF key k
+	 * 
 	 * @return Returns generated integer, PRF key k
 	 */
 	private int generatePRFKeyK()
@@ -458,6 +461,7 @@ public class MainWindow extends JFrame
 
 	/**
 	 * Calculates the authentication value for a given block of the input file
+	 * 
 	 * @param ba A byte array block of the erasure encoded file
 	 * @return Returns the generated authentication value
 	 */
@@ -465,7 +469,7 @@ public class MainWindow extends JFrame
 	{
 		long authenticator;
 		long blockXAlpha = 0;
-		
+
 		try
 		{
 			for (byte b : ba)
@@ -474,13 +478,14 @@ public class MainWindow extends JFrame
 		{
 			ex.printStackTrace();
 		}
-		
+
 		authenticator = _key + blockXAlpha;
 		return authenticator;
 	}
-	
+
 	/**
 	 * Accepts Q, encodedSliceBytes and list of authenticators and calculates sigma and mu to be returned to the verifier
+	 * 
 	 * @param Q The randomly generated index-coefficient challenge
 	 * @param encodedSliceBytes The list of byte arrays that contain the file slices
 	 * @param listOfAuthenticators A list of authenticators used in calculating sigma
@@ -488,7 +493,7 @@ public class MainWindow extends JFrame
 	 */
 	private List<Long> prover(List<Integer> Q, List<byte[]> encodedSliceBytes, List<Long> listOfAuthenticators)
 	{
-		//System.out.println("EncodedSlices size: " + encodedSliceBytes.size());
+		// System.out.println("EncodedSlices size: " + encodedSliceBytes.size());
 		List<Long> response = new ArrayList<Long>();
 		try
 		{
@@ -498,11 +503,11 @@ public class MainWindow extends JFrame
 			for (int i = 0; i < listOfAuthenticators.size(); i++)
 				sigma += Q.get(i) * listOfAuthenticators.get(i);
 
-			/* ///////////////////////////////////////////////////////////////////////////// */			
-			
+			/* ///////////////////////////////////////////////////////////////////////////// */
+
 			// Calculate mu as part of the response to be sent back to the verifier
 			long mu = 0;
-			
+
 			for (int i = 0; i < Q.size(); i++)
 			{
 				byte[] ba = encodedSliceBytes.get(i);
@@ -510,21 +515,22 @@ public class MainWindow extends JFrame
 				for (byte b : ba)
 					mu += coefficient * b;
 			}
-			
+
 			response.add(sigma);
 			response.add(mu);
 			tb_mu.setText(String.valueOf(mu));
-			
+
 		} catch (Exception ex)
 		{
 			ex.printStackTrace();
 		}
-		
+
 		return response;
 	}
-	
+
 	/**
 	 * Accepts the response from the Prover (sigma & mu), calculate verifySigma and compare them
+	 * 
 	 * @param response List that contains sigma and mu
 	 * @param Q The list of coefficients used to calculate verifySigma
 	 * @return Returns a bool to determine whether the sigmas match
@@ -534,15 +540,15 @@ public class MainWindow extends JFrame
 		long sigma = response.get(0);
 		long mu = response.get(1);
 		long keyXCoefficient = 0;
-	
+
 		for (int i : Q)
 			keyXCoefficient += _key * i;
-		
+
 		long verifySigma = _alpha * mu + keyXCoefficient;
-		
+
 		tb_sigma.setText(String.valueOf(sigma));
 		tb_calculatedSigma.setText(String.valueOf(verifySigma));
-		
+
 		if (sigma == verifySigma)
 			return true;
 		else
